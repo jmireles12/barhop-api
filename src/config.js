35ffe790e2054148bd83bd26b6c9b96efb6d9951
Juan Mireles <jmireles12@gmail.com>
2021-01-22 21:02:36 -0600

    test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/barhop',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://epcifomwotivdd:2fdc67e3d42c5db5e57f921c8d3a16592cd699e4ad4191496c6f92b6c6e33bb4@ec2-54-225-18-166.compute-1.amazonaws.com:5432/d5l6386ig8kvr9',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/barhop-test'
 }

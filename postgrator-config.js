a345c7eb43c267ee33961fad45feb6c0a02427f0
Juan Mireles <jmireles12@gmail.com>
2021-02-03 12:34:13 -0600

    test

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,8 @@ console.log(process.env.SSL);
 
 module.exports = {
     "migrationDirectory": "migrations",
+    ssl: process.env.NODE_ENV === "production",
     "driver": "pg",
-    "ssl": !!process.env.SSL,
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,

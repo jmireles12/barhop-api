13f9e61721631b5377b9420c494f261be7fb0b25
Juan Mireles <jmireles12@gmail.com>
2021-02-03 07:32:37 -0600

    added different ssl configuration

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,7 +4,9 @@ console.log(process.env.SSL);
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "ssl": !!process.env.SSL,
+    "ssl": {
+        rejectUnauthorized: false
+      },
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,

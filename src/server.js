773022f71362a4224b6e1f48be9ae1ee50ceb885
Juan Mireles <jmireles12@gmail.com>
2021-02-03 13:39:22 -0600

    test

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
 const pg = require('pg'); 
-pg.defaults.ssl = process.env.NODE_ENV === "production"
+pg.defaults.ssl = true;
 
 const db = knex({
     client: 'pg',
